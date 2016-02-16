0a414b5dbda06d5261a4bf6c9ec49c086ac74464
Alberto Vazquez <beto4812@hotmail.com>
2016-02-16 17:20:53 +0000

    Fixes

--- src/universityOfEdinburg/semanticWebSystems/MyParser.java
+++ src/universityOfEdinburg/semanticWebSystems/MyParser.java
@@ -38,10 +38,12 @@ public class MyParser {
 
 	public void init() {
 		namespaces.put("gn", "http://sws.geonames.org/");
+		namespaces.put("go", "http://www.geonames.org/ontology#");
 		namespaces.put("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
 		namespaces.put("rdfs", "http://www.w3.org/2000/01/rdf-schema#");
-		namespaces.put("foaf", "http://xmlns.com/foaf/spec/");
+		namespaces.put("foaf", "http://xmlns.com/foaf/spec/#");
 		namespaces.put("my", "http://vocab.inf.ed.ac.uk/sws/s1568644/");
+		namespaces.put("mo", "http://vocab.inf.ed.ac.uk/sws/s1568644/ontology#");
 		namespaces.put("db", "http://dbpedia.org/resource/");
 		namespaces.put("xsd", "http://www.w3.org/2001/XMLSchema#");
 	}
@@ -59,12 +61,10 @@ public class MyParser {
 		}
 
 		prefixes();
-		//myDefinitions();
 		mainContent();
 
-		//RDFWriter rdfW = m.getWriter();
-		//m.write(writer, "RDF/XML-ABBREV");
 		m.write(writer, "TURTLE");
+		//m.write(writer, "RDF/XML-ABBREV");
 		writer.close();
 	}
 
@@ -72,24 +72,9 @@ public class MyParser {
 
 		Set<String> it = namespaces.keySet();
 		for (String temp : it) {
-			//writer.println("@prefix " + temp + ": " + namespaces.get(temp));
 			System.out.println("temp: "+temp+" namespaces.get(temp): "+namespaces.get(temp));
 			m.setNsPrefix(temp, namespaces.get(temp));
 		}
-	}
-
-	public void myDefinitions() {
-		// Area Code
-		temp = m.createResource(namespaces.get("my")+"areaCode");
-		temp.addProperty(m.createProperty(namespaces.get("rdf")+"type"), m.createProperty(namespaces.get("rdfs")+"Property"));
-		temp.addProperty(m.createProperty(namespaces.get("rdfs")+"label"), "London area code of a feature");
-		temp.addProperty(m.createProperty(namespaces.get("rdfs")+"domain"), m.createProperty(namespaces.get("gn")+"Feature"));
-
-		// Population Percentage
-		temp = m.createResource(namespaces.get("my")+"populationPercentage");
-		temp.addProperty(m.createProperty(namespaces.get("rdf")+"type"), namespaces.get("rdfs")+"Property");
-		temp.addProperty(m.createProperty(namespaces.get("rdfs")+"label"), "Population percentage of a group");
-		temp.addProperty(m.createProperty(namespaces.get("rdfs")+"domain"), namespaces.get("foaf")+"Group");
 	}
 
 	/**
@@ -100,7 +85,7 @@ public class MyParser {
 		String line = null;
 		try {
 			br = new BufferedReader(new FileReader("religion-ward-2001.csv"));
-			//line = br.readLine();
+			line = br.readLine();
 			//line = br.readLine();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -120,7 +105,7 @@ public class MyParser {
 				e.printStackTrace();
 			}
 
-			if (line == null) {
+			if (line == null) { // || rowId ==3
 				break;
 			}
 
@@ -139,21 +124,22 @@ public class MyParser {
 				}
 
 				temp = m.createResource(namespaces.get("my")+"A" + rowId);
+				temp.addProperty(m.createProperty(namespaces.get("rdfs")+"type"), m.createResource(namespaces.get("go")+"Feature"));
 
 				temp.addProperty(m.createProperty(namespaces.get("rdfs")+"label"), m.createTypedLiteral(parts[2]));
-				temp.addProperty(m.createProperty(namespaces.get("rdf")+"type"), m.createResource(namespaces.get("gn")+"Feature"));
-				temp.addProperty(m.createProperty(namespaces.get("my")+"areaCode"), m.createTypedLiteral(parts[0]));
+
+				temp.addProperty(m.createProperty(namespaces.get("mo")+"areaCode"), m.createTypedLiteral(parts[0]));
 
 				for (int i = 0; i <= 8; i++) {
 					createPopulationResource(i, parts[4 + i], parts[13 + i]);
 					temp.addProperty(m.createProperty(namespaces.get("foaf")+"Group"), m.createResource(namespaces.get("my")+"P" + popId));
 				}
 				if (boroughGeoNameId.get(parts[1]) != -1) {
-					temp.addProperty(m.createProperty(namespaces.get("gn")+"parentFeature"),
+					temp.addProperty(m.createProperty(namespaces.get("go")+"parentFeature"),
 							m.createResource(namespaces.get("gn")+boroughGeoNameId.get(parts[1])));
 				}
 
-				temp.addProperty(m.createProperty(namespaces.get("gn")+"population"), m.createTypedLiteral(Integer.parseInt(parts[3])));
+				temp.addProperty(m.createProperty(namespaces.get("go")+"population"), m.createTypedLiteral(Integer.parseInt(parts[3])));
 			}
 			firstLine = false;
 		}
@@ -171,13 +157,14 @@ public class MyParser {
 		pop = m.createResource(namespaces.get("my")+"P" + popId);
 
 		if (religions[relIndex] != "") {
-			pop.addProperty(m.createProperty(namespaces.get("db")+"religion"), m.createResource(namespaces.get("db")+ religions[relIndex]));
+			pop.addProperty(m.createProperty(namespaces.get("db")+"Religion"), m.createResource(namespaces.get("db")+ religions[relIndex]));
 		} else {
-			pop.addProperty(m.createProperty(namespaces.get("db")+"religion"), "");
+			pop.addProperty(m.createProperty(namespaces.get("db")+"Religion"), "");
 		}
 
-		pop.addProperty(m.createProperty(namespaces.get("my")+"populationPercentage"), m.createTypedLiteral(Double.parseDouble(percentage)));
-		pop.addProperty(m.createProperty(namespaces.get("gn")+"population"), m.createTypedLiteral(Integer.parseInt(population)));
+		pop.addProperty(m.createProperty(namespaces.get("rdfs")+"type"), m.createResource(namespaces.get("foaf")+"Group"));
+		pop.addProperty(m.createProperty(namespaces.get("mo")+"populationPercentage"), m.createTypedLiteral(Double.parseDouble(percentage)));
+		pop.addProperty(m.createProperty(namespaces.get("go")+"population"), m.createTypedLiteral(Integer.parseInt(population)));
 	}
 
 }
